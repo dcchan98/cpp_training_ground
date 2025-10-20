b13c5375e2d1f283e30475156f844d3b3273c8b5
dcchan98 <dcchan98@gmail.com>
2025-10-20 21:36:28 +0800

    upgrade print to be able to take in variadic number of arguments

--- dc_notes.cpp
+++ dc_notes.cpp
@@ -4,12 +4,17 @@
 #include "cp_util/dc_funcs.hpp"
 #include "include/cpprint.hpp"
 using namespace cpprint;
-#define print(x) \
+#define print(...) \
 do { \
-std::cout << "Line " << __LINE__ << " : " << #x << " =============================================================\n"; \
-pprint(x); \
-std::cout << "=====================================================================================================\n"; \
-} while(0)
+    constexpr int width = 80; \
+    std::string line(width, '='); \
+    std::cout << "Line " << __LINE__ << " : " << #__VA_ARGS__ << "\n" << line << "\n"; \
+    variadic_print(__VA_ARGS__); \
+    std::cout << line << "\n"; \
+} while(0);
+
+template<typename... Args>
+void variadic_print(const Args &... args) { ((std::cout << generate_container_string_recursively(args) << "\n"), ...); }
 
 #include <bits/stdc++.h>
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 #include "cp_util/dc_funcs.hpp"
 #include "include/cpprint.hpp"
 using namespace cpprint;
-#define print(x) \
+#define print(...) \
 do { \
-constexpr int width = 80; \
-std::string line(width, '='); \
-std::cout << "Line " << __LINE__ << " : " << #x << "\n" << line << "\n"; \
-pprint(x); \
-std::cout << line << "\n"; \
+    constexpr int width = 80; \
+    std::string line(width, '='); \
+    std::cout << "Line " << __LINE__ << " : " << #__VA_ARGS__ << "\n" << line << "\n"; \
+    variadic_print(__VA_ARGS__); \
+    std::cout << line << "\n"; \
 } while(0);
+
+template<typename... Args>
+void variadic_print(const Args &... args) { ((std::cout << generate_container_string_recursively(args) << "\n"), ...); }
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -19,7 +22,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target) {
         unordered_map<int, int> seen; // number -> index
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
@@ -32,8 +35,8 @@ public:
     }
 };
 
-int main(){
-    vector<int> nums = {1,2,3,10};
+int main() {
+    vector<int> nums = {1, 2, 3, 10};
     int target = 11;
 
     Solution s;
@@ -41,6 +44,7 @@ int main(){
     // 1) Add print statements for seen, and for nums
     // 2) showcase 2 forge
     // 3) showcase debug mode
-    print(s.twoSum(nums,11));
-
+    vector<vector<int> > dp(5, vector<int>(6, -1));
+    print("ab", "cd", dp)
+    print("ab", "cd", dp)
 }
